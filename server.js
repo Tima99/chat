9a3b66e455b04e2f1bc1738be1969af98364548c
Amit <amit208201@gmail.com>
2022-03-04 20:23:54 +0530

    Server Listening Changed and Express use public folder as static files(i.e, index.html).

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ import connectDB from "./connectDB"
 import routes , { getRoutes } from "./routes"
 import { errorHandler } from "./middlewares"
 import cookieParser from "cookie-parser"
+import { Server } from "http"
 
-const app = express()
+const app    = express()
+const server = Server(app)
 
 /// express uses middlewares 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static('public')) //serves static files
 app.use('/api' , routes);    // post routes
 app.use('/api' , getRoutes); // get  routes  
 app.use(errorHandler)
 
 connectDB
-.then ( () => app.listen(PORT , () => console.log(`DB Connected |\nServer started on ${PORT}...`)) )
+.then ( () => server.listen(PORT , () => console.log(`DB Connected |\nServer started on ${PORT}...`)) )
 .catch( () => console.error('Error while connecting with DB.') )
